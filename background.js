478eecc581151c405d59eec1887fc909d2ab399e
Steven <stevendeleonwork@gmaill.com>
2022-03-01 17:49:51 -0800

    formatted using prettier

--- background.js
+++ background.js
@@ -1,10 +1,10 @@
 function getClickHandler(info, tab) {
-  chrome.tabs.sendMessage(tab.id, {text: "test-files-are-fun"});
-};
+  chrome.tabs.sendMessage(tab.id, { text: "test-files-are-fun" });
+}
 
 chrome.contextMenus.create({
-"title" : "Replace , / - with .",
-"type" : "normal",
-"contexts" : ["editable"],
-"onclick" : getClickHandler
+  title: "Replace , / - with .",
+  type: "normal",
+  contexts: ["editable"],
+  onclick: getClickHandler,
 });
--- content.js
+++ content.js
@@ -1,16 +1,16 @@
 chrome.runtime.onMessage.addListener(function (request) {
-    replaceSelectedText(document.activeElement, request.text);
+  replaceSelectedText(document.activeElement, request.text);
 });
 
 function replaceSelectedText(elem, text) {
-    var start = elem.selectionStart;
-    var end = elem.selectionEnd;
-    console.log(elem.value)
-    // elem.value = elem.value.slice(0, start) + elem.value + elem.value.substr(end);
-    elem.value = elem.value.replaceAll("-", ".");
-    elem.value = elem.value.replaceAll(",", ".");
-    elem.value = elem.value.replaceAll("/", ".");
-    // elem.value = elem.value.replaceAll(/\s+/g, '');
-    elem.selectionStart = start + text.length;
-    elem.selectionEnd = elem.selectionStart;
+  var start = elem.selectionStart;
+  var end = elem.selectionEnd;
+  console.log(elem.value);
+  // elem.value = elem.value.slice(0, start) + elem.value + elem.value.substr(end);
+  elem.value = elem.value.replaceAll("-", ".");
+  elem.value = elem.value.replaceAll(",", ".");
+  elem.value = elem.value.replaceAll("/", ".");
+  // elem.value = elem.value.replaceAll(/\s+/g, '');
+  elem.selectionStart = start + text.length;
+  elem.selectionEnd = elem.selectionStart;
 }
